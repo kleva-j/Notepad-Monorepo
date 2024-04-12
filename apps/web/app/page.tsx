9e409c522cfee37a2ae2afe9d4cddedd767cf4aa
kleva-j <kasmickleva@gmail.com>
2024-04-12 21:15:00 +0100

    chore: delete obsolete files, tsconfig paths, install dependencies

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,14 @@
-import { Badge } from "@repo/ui/components/ui/badge";
-
 export default function Page(): JSX.Element {
   return (
-    <main>
-      <div className="flex gap-2">
-        <Badge>Badge</Badge>
-        <Badge variant="secondary">Secondary</Badge>
-        <Badge variant="outline">Outline</Badge>
-        <Badge variant="destructive">Destructive</Badge>
-      </div>
+    <main className="flex w-full h-screen relative">
+      <section className="flex flex-col w-full p-10 ml-20 gap-5">
+        <h1 className="text-4xl text-neutral-200">Dashboard</h1>
+        <div className="w-full h-80 border border-neutral-500/50 rounded" />
+        <div className="flex flex-row gap-5 w-full">
+          <div className="border-neutral-500/50 border rounded h-60 w-1/2 bg-neutral-800/20" />
+          <div className="border-neutral-500/50 border rounded h-60 w-1/2 bg-neutral-800/20" />
+        </div>
+      </section>
     </main>
   );
 }
