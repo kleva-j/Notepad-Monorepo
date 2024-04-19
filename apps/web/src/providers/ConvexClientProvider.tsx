5fb838e01af99a3cc7240d8b876c46169c5ab175
kleva-j <kasmickleva@gmail.com>
2024-04-19 14:18:59 +0100

    feat: setup errorboundary around the client providers

--- apps/web/src/providers/ConvexClientProvider.tsx
+++ apps/web/src/providers/ConvexClientProvider.tsx
@@ -4,18 +4,22 @@ import type { PropsWithChildren } from "react";
 
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ErrorBoundary } from "@/components/ErrorBoundary";
 import { ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({ children }: PropsWithChildren) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
-    >
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        {children}
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
+    // NOTE: Once you get Clerk working you can remove this error boundary
+    <ErrorBoundary>
+      <ClerkProvider
+        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      >
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          {children}
+        </ConvexProviderWithClerk>
+      </ClerkProvider>
+    </ErrorBoundary>
   );
 }
