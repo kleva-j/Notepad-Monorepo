980f706497589d5d28bf2838eee2b77a732c11ba
kleva-j <kasmickleva@gmail.com>
2024-04-19 14:16:51 +0100

    feat: setup clerk and convex providers

---
+++ apps/web/src/providers/ConvexClientProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import type { PropsWithChildren } from "react";
+
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClientProvider({ children }: PropsWithChildren) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
