b71e7c7e85205e76e62e64754d6d57541ab03d53
kleva-j <kasmickleva@gmail.com>
2024-04-22 10:55:56 +0100

    feat: setup landing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,21 @@
-import { Nav } from "@/components/Nav";
+import { CTA } from "@/components/layout/CTA";
+import { Content } from "@/components/layout/Content";
+import { Footer } from "@/components/layout/Footer";
+import { Header } from "@/components/layout/Header";
+import { Hero } from "@/components/layout/Hero";
+import { Team } from "@/components/layout/Team";
 
 export default function Page(): JSX.Element {
   return (
-    <main className="flex w-full h-screen relative">
-      <Nav />
-      <section className="flex flex-col w-full ml-20 gap-5 dark:bg-neutral-900" />
+    <main className="flex w-full h-screen relative flex-col bg-white dark:bg-gray-950">
+      <Header />
+      <div className="flex flex-col">
+        <Hero />
+        <Content />
+        <Team />
+        <CTA />
+      </div>
+      <Footer />
     </main>
   );
 }
