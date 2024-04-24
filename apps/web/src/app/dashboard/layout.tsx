ef95668d311805c5f9ec62655c2f296e057e5f6f
kleva-j <kasmickleva@gmail.com>
2024-04-24 18:04:03 +0100

    feat: create parallel route for header in dashboard

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
-import type { PropsWithChildren } from "react";
+import type { PropsWithChildren, ReactNode } from "react";
 
 import { Nav } from "@/components/Nav";
 
-export default function Layout({ children }: PropsWithChildren) {
+type LayoutProps = PropsWithChildren & { header: ReactNode };
+
+export default function Layout({ children, header }: LayoutProps) {
   return (
     <main className="flex w-full h-screen relative">
       <Nav />
-      <section className="flex flex-col w-full ml-20 gap-5 dark:bg-neutral-900">
+      <section className="flex flex-col w-full ml-20 bg-neutral-200/50 dark:bg-neutral-900">
+        {header}
         {children}
       </section>
     </main>
