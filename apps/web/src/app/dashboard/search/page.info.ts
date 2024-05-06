e48ecd8eeb18c4814632c403aaa79401bf6c3a96
kleva-j <kasmickleva@gmail.com>
2024-05-06 23:58:55 +0100

    feat: add popover, calendar, card, radio-group, toast, toaster, separator components

--- apps/web/src/app/dashboard/search/page.info.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const Route = {
-  name: "DashboardSearch",
-  params: z.object({}),
-  search: z.object({
-    q: z.string().optional(),
-  }),
-};
--- apps/web/src/app/dashboard/search/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div>This should be a search page</div>;
-}
