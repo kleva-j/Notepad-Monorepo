5b537fddf010732bf488815187e32d70da7f1bd5
kleva-j <kasmickleva@gmail.com>
2024-06-16 00:23:32 +0100

    feat: implement preloaded queries for tasks

--- apps/web/src/app/dashboard/tasks/page.tsx
+++ apps/web/src/app/dashboard/tasks/page.tsx
@@ -1,9 +1,11 @@
-import { CreateTaskForm } from "@/components/tasks/CreateTaskForm";
-import { CreateTaskBtn } from "@/components/tasks/CreateTaskBtn";
+import { CreateTaskModal } from "@/components/tasks/CreateTaskModal";
 import { Separator } from "@repo/ui/components/ui/separator";
+import { api } from "@repo/backend/convex/_generated/api";
 import { KanbanBoard } from "@/components/tasks/Kanban";
 import { TableView } from "@/components/tasks/Table";
 import { ListView } from "@/components/tasks/List";
+import { preloadQuery } from "convex/nextjs";
+import { getAuthToken } from "@/lib/auth";
 import {
   AlignVerticalDistributeStart,
   AlignStartHorizontal,
@@ -16,15 +18,16 @@ import {
   Tabs,
 } from "@repo/ui/components/ui/tabs";
 
-export default function TasksPage() {
+export default async function TasksPage() {
+  const token = await getAuthToken();
+  const tasks = await preloadQuery(api.notes.getAllActionItems, {}, { token });
+
   return (
     <main className="flex min-h-[calc(100vh_-_theme(spacing.20))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 overflow-y-auto">
       <div className="mx-auto flex justify-between w-full max-w-[80rem] gap-2">
         <h1 className="text-3xl font-medium">Tasks</h1>
 
-        <CreateTaskBtn>
-          <CreateTaskForm />
-        </CreateTaskBtn>
+        <CreateTaskModal />
       </div>
 
       <div className="mx-auto w-full max-w-[80rem]">
@@ -53,7 +56,7 @@ export default function TasksPage() {
             <ListView />
           </TabsContent>
           <TabsContent value="table">
-            <TableView />
+            <TableView preloadedTasks={tasks} />
           </TabsContent>
         </Tabs>
       </div>
