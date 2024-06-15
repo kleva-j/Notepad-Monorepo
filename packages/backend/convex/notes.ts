b1128bd40248e95ede7820375b548b5d582419af
kleva-j <kasmickleva@gmail.com>
2024-06-15 23:43:13 +0100

    feat: implement crud features for action-items

--- packages/backend/convex/notes.ts
+++ packages/backend/convex/notes.ts
@@ -2,6 +2,15 @@ import { ConvexError, v } from "convex/values";
 
 import { mutateWithUser, queryWithUser } from "./utils";
 
+const actionItemSchema = v.object({
+  title: v.string(),
+  description: v.string(),
+  status: v.string(),
+  priority: v.string(),
+  column: v.string(),
+  tags: v.array(v.string()),
+});
+
 export const generateUploadUrl = mutateWithUser({
   args: {},
   handler: async (ctx) => {
@@ -45,10 +54,9 @@ export const getNote = queryWithUser({
 // Get all notes for a specific user
 export const getNotes = queryWithUser({
   args: {},
-  handler: async (ctx) => {
-    const userId = ctx.userId;
+  handler: async ({ userId, db }) => {
     if (!userId) return null;
-    const notes = await ctx.db
+    const notes = await db
       .query("notes")
       .withIndex("by_userId", (q) => q.eq("userId", userId))
       .collect();
@@ -71,6 +79,108 @@ export const deleteNote = mutateWithUser({
       }
       const note = await ctx.db.delete(id);
       return note;
+    }
+  },
+});
+
+export const getAllActionItems = queryWithUser({
+  args: {},
+  handler: async (ctx) => {
+    const userId = ctx.userId;
+
+    if (!userId) return null;
+
+    const notes = await ctx.db
+      .query("notes")
+      .withIndex("by_userId", (q) => q.eq("userId", userId))
+      .collect();
+
+    const actionItems = [];
+
+    for (const note of notes) {
+      const item = await ctx.db
+        .query("actionItems")
+        .withIndex("by_noteId", (q) => q.eq("noteId", note._id))
+        .collect();
+      actionItems.push(...item);
+    }
+
+    return actionItems;
+  },
+});
+
+export const getActionItemById = queryWithUser({
+  args: {
+    id: v.id("actionItems"),
+  },
+  handler: async (ctx, { id }) => {
+    if (!id) return null;
+
+    const actionItem = await ctx.db.get(id);
+
+    if (!actionItem) return null;
+
+    const note = await ctx.db.get(actionItem?.noteId);
+
+    if (note) {
+      if (note?.userId !== ctx.userId) throw new ConvexError("Not your note.");
+      return actionItem;
+    }
+  },
+});
+
+export const createActionItem = mutateWithUser({
+  args: {
+    noteId: v.id("notes"),
+    data: actionItemSchema,
+  },
+  handler: async ({ userId, db }, { noteId, data }) => {
+    if (!noteId) return null;
+
+    if (!userId) return null;
+
+    const note = await db.get(noteId);
+
+    if (!note) return null;
+
+    const id = await db.insert("actionItems", {
+      noteId,
+      userId,
+      ...data,
+    });
+
+    const actionItem = await db.get(id);
+
+    return actionItem;
+  },
+});
+
+export const updateActionItem = mutateWithUser({
+  args: {
+    id: v.id("actionItems"),
+    data: v.object({
+      title: v.optional(v.string()),
+      description: v.string(),
+      status: v.string(),
+      priority: v.optional(v.string()),
+      column: v.optional(v.string()),
+      tags: v.optional(v.array(v.string())),
+    }),
+  },
+  handler: async (ctx, { id, data }) => {
+    if (!id) return null;
+
+    const actionItem = await ctx.db.get(id);
+
+    if (!actionItem) return null;
+
+    const note = await ctx.db.get(actionItem?.noteId);
+
+    if (note) {
+      if (note?.userId !== ctx.userId) throw new ConvexError("Not your note.");
+      await ctx.db.patch(id, { ...actionItem, ...data });
+      const updatedActionItem = await ctx.db.get(id);
+      return updatedActionItem;
     }
   },
 });
