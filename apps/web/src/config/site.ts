d7586a81c4b6469395393acaba493ced69878d7d
kleva-j <kasmickleva@gmail.com>
2024-06-18 23:40:35 +0100

    feat: add a site config file

---
+++ apps/web/src/config/site.ts
@@ -0,0 +1,8 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Notepad-GPT",
+  description: "An AI powered voice note-taking app, converting your voice notes into actionable summaries and clear action items.",
+  url: "http://localhost:3000",
+  links: { github: "https://github.com/kleva-j/Notepad-Monorepo" },
+};
