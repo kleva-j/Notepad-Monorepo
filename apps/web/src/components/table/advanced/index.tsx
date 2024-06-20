1b8b192c8414e78f3d1960cb4d7a66ad15271781
kleva-j <kasmickleva@gmail.com>
2024-06-20 19:32:15 +0100

    feat: organise/grouped file exports

---
+++ apps/web/src/components/table/advanced/index.tsx
@@ -0,0 +1,5 @@
+export * from "./data-table-advanced-faceted-filter";
+export * from "./data-table-advanced-toolbar";
+export * from './data-table-filter-combobox';
+export * from "./data-table-multi-filter";
+export * from './data-table-filter-item';
