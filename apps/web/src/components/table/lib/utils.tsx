91eb2da179b90d449e3ab811156b2075d3587b77
kleva-j <kasmickleva@gmail.com>
2024-06-20 19:37:11 +0100

    feat: utility functions: getErrorMessage, showErrorToast and formatDate

--- apps/web/src/components/table/lib/utils.tsx
+++ apps/web/src/components/table/lib/utils.tsx
@@ -1,4 +1,10 @@
-import type{ Task } from "@/types/tasks";
+import type { DataTableFilterField, IconProps } from "@/types";
+import type { Priority, Status, Task } from "@/types/tasks";
+
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { PRIORITY, STATUS } from "@/lib/constant";
+import { toast } from "sonner";
+import { z } from "zod";
 import {
   QuestionMarkCircledIcon,
   CheckCircledIcon,
@@ -16,15 +22,16 @@ import {
  * @param status - The status of the task.
  * @returns {React.ComponentType} A React component representing the status icon.
  */
-export function getStatusIcon(status: Task["status"]): React.ComponentType {
-  const statusIcons: { [key in Task["status"]]: React.ComponentType } = {
-    cancelled: CrossCircledIcon,
-    done: CheckCircledIcon,
-    doing: StopwatchIcon,
-    pending: QuestionMarkCircledIcon,
-  };
+export function getStatusIcon(status: Status): React.ComponentType<IconProps> {
+  const IconComponent =
+    {
+      cancelled: CrossCircledIcon,
+      done: CheckCircledIcon,
+      doing: StopwatchIcon,
+      pending: QuestionMarkCircledIcon,
+    }[status] || CrossCircledIcon;
 
-  return statusIcons[status] || CircleIcon;
+  return (props: any) => <IconComponent {...props} />;
 }
 
 /**
@@ -32,15 +39,75 @@ export function getStatusIcon(status: Task["status"]): React.ComponentType {
  * @param priority - The priority of the task.
  * @returns {React.ComponentType} A React component representing the priority icon.
  */
-export function getPriorityIcon(
-  priority: Task["priority"]
-): React.ComponentType {
-  const priorityIcons: { [key in Task["priority"]]: React.ComponentType } = {
-    high: ArrowUpIcon,
-    low: ArrowDownIcon,
-    normal: ArrowRightIcon,
-    urgent: ArrowLeftIcon,
-  };
+export function getPriorityIcon(priority: Priority): React.ComponentType<IconProps> {
+  const IconComponent =
+    {
+      high: ArrowUpIcon,
+      low: ArrowDownIcon,
+      normal: ArrowRightIcon,
+      urgent: ArrowLeftIcon,
+    }[priority] || CircleIcon;
 
-  return priorityIcons[priority] || CircleIcon;
+  return (props: any) => <IconComponent {...props} />;
 }
+
+export function getErrorMessage(err: unknown) {
+  const unknownError = "Something went wrong, please try again later.";
+
+  if (err instanceof z.ZodError) {
+    const errors = err.issues.map((issue) => {
+      return issue.message;
+    });
+    return errors.join("\n");
+  } else if (err instanceof Error) {
+    return err.message;
+  } else if (isRedirectError(err)) {
+    throw err;
+  } else {
+    return unknownError;
+  }
+}
+
+export function showErrorToast(err: unknown) {
+  const errorMessage = getErrorMessage(err);
+  return toast.error(errorMessage);
+}
+
+  /**
+   * This component can render either a faceted filter or a search filter based on the `options` prop.
+   *
+   * @prop options - An array of objects, each representing a filter option. If provided, a faceted filter is rendered. If not, a search filter is rendered.
+   *
+   * Each `option` object has the following properties:
+   * @prop {string} label - The label for the filter option.
+   * @prop {string} value - The value for the filter option.
+   * @prop {React.ReactNode} [icon] - An optional icon to display next to the label.
+   * @prop {boolean} [withCount] - An optional boolean to display the count of the filter option.
+   */
+  export const filterFields: DataTableFilterField<Task>[] = [
+    {
+      label: "Title",
+      value: "title",
+      placeholder: "Filter titles...",
+    },
+    {
+      label: "Status",
+      value: "status",
+      options: STATUS.map((status) => ({
+        label: status[0]?.toUpperCase() + status.slice(1),
+        value: status,
+        icon: getStatusIcon(status),
+        withCount: true,
+      })),
+    },
+    {
+      label: "Priority",
+      value: "priority",
+      options: PRIORITY.map((priority) => ({
+        label: priority[0]?.toUpperCase() + priority.slice(1),
+        value: priority,
+        icon: getPriorityIcon(priority),
+        withCount: true,
+      })),
+    },
+  ]
--- apps/web/src/lib/utils.tsx
+++ apps/web/src/lib/utils.tsx
@@ -24,3 +24,15 @@ export const getTableDataByColumn = () => {
   }, listColumn);
   return cardsByColumn;
 };
+
+export function formatDate(
+  date: Date | string | number,
+  opts: Intl.DateTimeFormatOptions = {}
+) {
+  return new Intl.DateTimeFormat("en-US", {
+    month: opts.month ?? "long",
+    day: opts.day ?? "numeric",
+    year: opts.year ?? "numeric",
+    ...opts,
+  }).format(new Date(date));
+}
